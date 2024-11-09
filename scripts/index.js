6a9fac7b13ee31630f38f1e1007710e6fbd5ebb0
Patrick <Patrickvelaz@gmail.com>
2024-11-09 16:16:55 -0500

    check for error

--- scripts/index.js
+++ scripts/index.js
@@ -29,25 +29,66 @@ const initialCards = [
 ]
 
 const modal = document.querySelector(".modal")
-
+const editProfileModal = document.querySelector("#edit-profile-modal")
+const profileTitleInput = editProfileModal.querySelector("#modal__input_type_name")
+const profileDescriptionInput = editProfileModal.querySelector("#modal__input_type_description")
+const cardTemplate = document.querySelector("#card-template").content.querySelector(".card");
+const cardsList = document.querySelector(".cards__list");
 
 const profileEditBtn = document.querySelector(".profile__edit-button")
 const modalCloseBtn =  document.querySelector(".modal__close")
+const profileEditForm = document.querySelector(".profile__edit-form")
+
+const profileName = document.querySelector(".profile__name")
+const profileDescription = document.querySelector(".profile__description")
 
 
-function closeModal () {
+function closeModal (modal) {
     modal.classList.remove("modal_open")
 }
 
-function openModal () {
+function openModal (modal) {
     modal.classList.add("modal_open")
 }
 
+function handleProfileEditSubmit(evt) {
+    evt.preventDefault();
+    profileName.textContent = profileTitleInput.value
+    profileDescription.textContent = profileDescriptionInput.value
+
+    closeModal(editProfileModal)
+}
+
+function getCardElement(data) {
+    const cardElement = cardTemplate.cloneNode(true);
+    const cardImage = cardElement.querySelector(".card__image");
+    const cardTitle = cardElement.querySelector(".card__title");
+    cardImage.src = data.link;
+    cardImage.alt = data.name;
+    cardTitle.textContent = data.name;
+    console.log(cardImage,  cardTitle);
+
+    return cardElement;
+}
+
+function renderCard(data, wrap) {
+    console.log(data)
+    wrap.prepend(getCardElement(data))
+    
+}
 
 profileEditBtn.addEventListener("click", () => {
-   openModal()
+    profileTitleInput.value = profileName.textContent 
+    profileDescriptionInput.value = profileDescription.textContent
+   openModal(editProfileModal)
 })
 
 modalCloseBtn.addEventListener("click", () => {
-    closeModal()
+    closeModal(editProfileModal)
+})
+
+editProfileModal.addEventListener("submit", handleProfileEditSubmit)
+
+initialCards.forEach((data) => {
+    renderCard(data, cardsList)
 })
